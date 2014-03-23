dd95457d81d8803d843145968daedf0c45557e7d
ilansh <ilanshamir1@gmail.com>
2014-03-23 16:14:04 +0200

    training set done, begin validation

--- ex4/PolyFitter.py
+++ ex4/PolyFitter.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 TRAINING_SET_SIZE = 20
 VALIDATION_SET_SIZE = 101
 TEST_SET_SIZE = 100
+MAX_DEGREE = 15
 
 class PolyFitter:
 
@@ -26,18 +27,41 @@ class PolyFitter:
             self.validationSet.append((float(domain[i]), float(labels[i])))
         for i in range(TRAINING_SET_SIZE + VALIDATION_SET_SIZE, TEST_SET_SIZE):
             self.testSet.append((float(domain[i]), float(labels[i])))
+      
+    def toMatrixForm(self, d, data, dataSize):
+        y = list(sample[1] for sample in data)
+        y = np.matrix(y).transpose()
+        X = np.zeros(shape=(d + 1, dataSize))
+        for i in range(d + 1):
+            for j in range(dataSize):
+                X[i][j] = data[j][0] ** i
+        X = np.matrix(X)
+        return X, y
           
     def train(self, d):
-        X = np.zeros(shape=(d + 1, TRAINING_SET_SIZE))
-        for i in range(d + 1):
-            for j in range(TRAINING_SET_SIZE):
-                X[i][j] = self.trainingSet[j][0] ** i
-        X = np.matrix(X)
-
+        X, y = self.toMatrixForm(d, self.trainingSet, TRAINING_SET_SIZE)
+        U, s, V = np.linalg.svd(X.transpose(), full_matrices = False)
+        S = np.diag(s)
+        return V * np.linalg.inv(S) * U.transpose() * y
+        
+    def validate(self, H):
+        X, y = toMatrixForm(d, self.validationSet, VALIDATION_SET_SIZE)
+        err = 0
+        for h in H:
+            for sample in self.validationSet:
+                err += (h * X[i] - y[i]) ** 2 
+    
+    def fit(self):
+        H = []
+        print self.train(5)
+        #for i in range(1, MAX_DEGREE + 1):
+        #    H.append(p.train(i))
+        
+    
 def main():
     p = PolyFitter()
     p.loadData('X.txt','Y.txt')
-    p.train(4)
+    p.fit()
     
 if __name__ == "__main__":
     main()
